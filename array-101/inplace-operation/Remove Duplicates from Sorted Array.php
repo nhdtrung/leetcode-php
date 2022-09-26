8a36901cabfe34e886615a1b647528ca7d8ec3db
Travis Nguyen <travisnguyenhoang@gmail.com>
2022-09-26 22:54:55 +0700

    Remove Duplicates from Sorted Array

---
+++ array-101/inplace-operation/Remove Duplicates from Sorted Array.php
@@ -0,0 +1,25 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function removeDuplicates(&$nums) {
+        if(sizeof($nums) == 0)
+            return 0;
+        
+        $i = 0;
+        for($j = 0; $j < sizeof($nums); $j++) {
+            if($nums[$i] == $nums[$j]) {
+                $nums[$i] = $nums[$j];
+            } else {
+                $nums[++$i] = $nums[$j];
+                $count++;
+            }
+        }
+        
+        return ++$i;
+    }
+}
